79d62d32fc0045b4da76ad1fda06e69876f9098e
dadeo <drobertwatson@gmail.com>
2015-12-18 00:57:21 -0600

    minor playground cleanup

--- src/test/groovy/com/github/dadeo/pdfbox/VisualPlaygroundTest.groovy
+++ src/test/groovy/com/github/dadeo/pdfbox/VisualPlaygroundTest.groovy
@@ -35,14 +35,13 @@ class VisualPlaygroundTest {
     static final float ONE_INCH = 72f
 
     private static final String LOREM_IPSUM = """|Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                           |tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                           |quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                           |consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                           |cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                           |proident, sunt in culpa qui officia deserunt mollit anim id est laborum g."""
+                                                 |tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                                 |quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                                 |consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                                 |cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                                 |proident, sunt in culpa qui officia deserunt mollit anim id est laborum g."""
         .stripMargin()
-        .replaceAll(/\r/, '')
-        .replaceAll(/\n/, '')
+        .replaceAll(/\r|\n/, '')
 
     private Closure<Float> toFloat = { Number number -> number.toFloat() }
     private Closure<DPoint> point = { Number x, Number y ->
